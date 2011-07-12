136bb85d83084b4de4af5b962859c7d753769c17
Kevin Le <solnovus@gmail.com>
2011-07-12 14:03:47 -0700

    add dcramer's devserver

--- settings.py
+++ settings.py
@@ -98,6 +98,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'codereview',
     'south',
+    'devserver',
     'debug_toolbar',
 )
 
@@ -105,6 +106,19 @@ AUTH_PROFILE_MODULE = 'codereview.Account'
 LOGIN_REDIRECT_URL = '/'
 
 UPLOAD_PY_SOURCE = os.path.join(os.path.dirname(__file__), 'upload.py')
+
+DEVSERVER_MODULES = (
+    'devserver.modules.sql.SQLRealTimeModule',
+    'devserver.modules.sql.SQLSummaryModule',
+    'devserver.modules.profile.ProfileSummaryModule',
+
+    # Modules not enabled by default
+    'devserver.modules.ajax.AjaxDumpModule',
+    'devserver.modules.profile.MemoryUseModule',
+    'devserver.modules.cache.CacheSummaryModule',
+    'devserver.modules.profile.LineProfilerModule',
+)
+
 
 DEBUG_TOOLBAR_PANELS = (
     'debug_toolbar.panels.version.VersionDebugPanel',
